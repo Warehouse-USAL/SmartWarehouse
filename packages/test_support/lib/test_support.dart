e131a9b1cdf4ffd9650d6bf5cc830e814410b5a6
Joaco Flores <joaquinflores0109@gmail.com>
2026-08-14 01:00:39 -0300

    feat(test_support): builders de entities compartidas y carga de fixtures

--- packages/test_support/lib/test_support.dart
+++ packages/test_support/lib/test_support.dart
@@ -1,4 +1,8 @@
 library test_support;
 
+export 'src/builders/money_builder.dart';
+export 'src/builders/product_builder.dart';
+export 'src/builders/stock_builder.dart';
+export 'src/fixtures.dart';
 export 'src/injector_harness.dart';
 export 'src/mocks.dart';
